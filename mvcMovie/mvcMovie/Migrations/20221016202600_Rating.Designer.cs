a3576844ca18e5f0bf7e665cf14be068df8dad53
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-10-16 16:38:59 -0400

    Add rating field for movies

--- mvcMovie/mvcMovie/Migrations/20221016202600_Rating.Designer.cs
+++ mvcMovie/mvcMovie/Migrations/20221016202600_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mvcMovie.Data;
 
@@ -11,9 +12,10 @@ using mvcMovie.Data;
 namespace mvcMovie.Migrations
 {
     [DbContext(typeof(mvcMovieContext))]
-    partial class mvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20221016202600_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- mvcMovie/mvcMovie/Models/Movie.cs
+++ mvcMovie/mvcMovie/Models/Movie.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mvcMovie.Models
@@ -15,5 +14,6 @@ namespace mvcMovie.Models
         public string? Genre { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string? Rating { get; set; }
     }
 }
